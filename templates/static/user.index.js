2b834b7c34ed8e277a4a2ab1e5c859cc13eb8f37
bbdk612 <sircenkom239@gmail.com>
2024-05-31 03:18:46 +0500

    make document with structure description

--- templates/static/user.index.js
+++ templates/static/user.index.js
@@ -1,18 +1,23 @@
 $(document).ready(() => {
-  $(".lesson__button-delete").click((e) => {
-    let id = e.target.id;
+    $(".lesson__button-delete").click((e) => {
+        let id = e.target.id;
 
-    $.ajax({
-      method: "POST",
-      url: `/lesson/delete/${id}`,
-      dataType: "json",
-      success: (data) => {
-        $(`li#lesson_${id}`).hide(20);
-        alert(data.message);
-      },
-      error: (xhr, status, error) => {
-        alert(xhr.responseText);
-      },
+        $.ajax({
+            method: "POST", url: `/lesson/delete/${id}`, dataType: "json", success: (data) => {
+                $(`li#lesson_${id}`).hide(20);
+                alert(data.message);
+            }, error: (xhr, status, error) => {
+                alert(xhr.responseText);
+            },
+        });
+
     });
-  });
+    $(".logout").click(() => {
+        $.ajax({
+            method: "POST", url: "/logout", dataType: "json", success: (data) => {
+                alert(data.message);
+                window.location.replace("/login");
+            },
+        });
+    });
 });
